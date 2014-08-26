58b2a7384d95a84ec6cc5aaeaeb4fcb84afbf1a4
DouglasH9 <douglasherman@Douglas-Hermans-MacBook-Pro.local>
2014-08-25 22:35:43 -0500

    jquery changes

--- chompScript.js
+++ chompScript.js
@@ -1,8 +1,20 @@
-$(document).ready(funciton() {
+$(document).ready(function() {
+	
+	
 	$('#button').click(function() {
 		console.log ('hello');
 		var itemAdd = $('input[name=listItems]').val()
 		$("#list").prepend('<div class="item">' + itemAdd + '</div>')
+
+	});
+
+	$('#listInput').keypress(function(event){
+		if(event.which == 13) {
+			console.log ("enter was pressed");
+			var itemAdd = $('input[name=listItems]').val()
+			$('#list').prepend('<div class="item">' + itemAdd + '</div>')
+		};
+
 
 	});
 });
