fc7abf6fc786d6eaff22602260601aa7bb4c00bf
DouglasH9 <douglasherman@douglasrmansmbp.gateway.2wire.net>
2014-08-26 23:10:03 -0500

    final form?

--- chompScript.js
+++ chompScript.js
@@ -1,23 +1,61 @@
 $(document).ready(function() {
 	
-	var check = 
-	
+	var xMark = '<span class="xMark"></span>'
+	var checkMark = '<span class="checkMark"></span>'
+
+//animates pacman
+	$('#pacman').mouseenter(function() {
+		$('#pacman').animate({'width':'12%'},200);
+		
+	});
+
+//adds item	on button click
 	$('#button').click(function() {
 		console.log ('hello');
 		var itemAdd = $('input[name=listItems]').val()
-		$("#goods").prepend('<li class="item">' + itemAdd + '</li>')
 
+		if (itemAdd == '') {
+				alert('Add an item ya dum dum!');
+		}
+		else {
+			$("#goods").prepend('<li class="item">' + xMark + itemAdd + checkMark + '</li>')
+			$('#listInput').val('')
+		}
 	});
-
+//adds item on enter
 	$('#listInput').keypress(function(event){
 		if(event.which == 13) {
 			console.log ("enter was pressed");
 			var itemAdd = $('input[name=listItems]').val()
-			$('#goods').prepend('<li class="item"><div class="ecks"></div>' + itemAdd + '<div class="check"></div></li>')
+
+			if (itemAdd == '') {
+				alert('Add an item ya dum dum!');
+			}
+				else {
+
+					$('#goods').prepend('<li class="item">' + xMark + itemAdd + checkMark + '</li>')
+					$('#listInput').val('')
+				}
 		};
 	});
-
-	$('.ecks').click(function(){
-		$(this).remove('.item')
+//checks off or unchecks list items
+	$(document).on('click' , '.xMark', function(){
+		$(this).closest('li').fadeOut('fast')
 	});
+
+	$(document).on('click' , '.checkMark', function(){
+		if ($(this).closest('li').hasClass('item')) {
+			$(this).closest('li').addClass('crossOut');
+			$(this).closest('li').removeClass('item');
+		}
+
+			else if ($(this).closest('li').hasClass('crossOut')) {
+				$(this).closest('li').removeClass('crossOut');
+				$(this).closest('li').addClass('item');
+			}
+
+		
+	});
+
+
 });
